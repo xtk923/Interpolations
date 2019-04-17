a1aa329a878839d8ff9b1ffa1da23a9d24ab330b
熊天锴 <Xiong@xiongtiankais-MacBook-Pro.local>
2019-04-17 22:15:54 +0800

    used Delaunay Triangulation

--- griddata.jl
+++ griddata.jl
@@ -1,11 +1,14 @@
 using LinearAlgebra
 using DataFrames
+using VoronoiDelaunay
+using GeometricalPredicates
+
 """
     griddata with linear interpolation which works similar to its matlab counterpart
 
     `griddata(x::Array{Real, 1}, y::Array{Real, 1}, v::Array{Real, 1}, xq::Array{Real, 1}, yq::Array{Real, 1})`
 """
-function griddata(x, y, v,
+function griddata(x::Array, y::Array, v::Array,
                   xq, yq)
     if !(length(x) == length(y) && length(y) == length(v))
         error("x, y and v must be in same dimension.")
@@ -14,93 +17,143 @@ function griddata(x, y, v,
         error("xq and yq must be in same dimension.")
     end
 
+    newX, xScale, xShift = normalizedToInterval(x, VoronoiDelaunay.min_coord,
+                                                VoronoiDelaunay.max_coord)
+    newY, yScale, yShift = normalizedToInterval(y, VoronoiDelaunay.min_coord,
+                                                VoronoiDelaunay.max_coord)
+
+    tess = DelaunayTessellation(2*length(x))
+    points = Point.(newX, newY)
+    push!(tess, points)
+    # tess is a Delaunay Trianglation
+    triangles = []
+    for t in tess
+        push!(triangles, Primitive(geta(t), getb(t), getc(t)))
+    end
+
     res = []
     for i = 1:length(xq)
-        dists = ((xq[i] .- x).^2 + (yq[i] .- y).^2).^0.5
-        point = DataFrame(X = xq[i], Y = yq[i], V = [missing], Dist = [0])
-        points = DataFrame(X = x, Y = y, V = v, Dist = dists)
-        sort!(points, :Dist)
-        # find the nearest three by (x,y)
-
-        # if all nearest points are on the left of quest point,
-        # drop the left most one.
-        vertices = points[1:3, :]
-        # the row at which we read the next point from points
-        splitRow = 4
-        while splitRow <= size(points, 1)
-            if !isInTriangle(point, vertices)
-                vertices = vertices[1:2, :]
-                append!(vertices, points[splitRow, :])
-                splitRow += 1
-            else
+        normalizedX = xq[i] / xScale - xShift
+        normalizedY = yq[i] / yScale - yShift
+        p = Point2D(normalizedX, normalizedY)
+        theTriangle = nothing
+        for t in triangles
+            if isInTriangle(p, t)
+                theTriangle = t
                 break
             end
         end
-        @debug splitRow
-        if splitRow <= size(points, 1) + 1
-            vertices = convert(Matrix, vertices[[:X, :Y, :V]])
 
-            # with three nearest points, get the expression of the plane,
-            # then find the z value of f(xq[i], yq[i])
-            D = det(vertices)
-            d = 1
-            a = -d/D * det(
-                hcat([1,1,1], vertices[:, 2:3])
-            )
-            b = -d/D * det(
-                hcat(vertices[:, 1], [1,1,1], vertices[:, 3])
-            )
-            c = -d/D * det(
-                hcat(vertices[:, 1:2], [1,1,1])
-            )
-            vq = (a*xq[i] + b*yq[i] + d) / -c
-            append!(res, vq)
+        if theTriangle != nothing
+            skipping = false
+            vertices = Array{Float64, 2}(undef, 3,3)
+            vertices[1,1] = getx(geta(theTriangle))
+            vertices[2,1] = getx(getb(theTriangle))
+            vertices[3,1] = getx(getc(theTriangle))
+            vertices[1,2] = gety(geta(theTriangle))
+            vertices[2,2] = gety(getb(theTriangle))
+            vertices[3,2] = gety(getc(theTriangle))
+            vertices[:,1] = reverseNormalizedToInterval(vertices[:,1], xScale, xShift)
+            vertices[:,2] = reverseNormalizedToInterval(vertices[:,2], yScale, yShift)
+
+            for r = 1:3
+                idxAccX = findall(x .≈ vertices[r, 1])
+                idxAccY = findall(y .≈ vertices[r, 2]) 
+                idxForZ = intersect(idxAccX, idxAccY)
+                if length(idxForZ) == 1
+                    vertices[r, 3] = v[idxAccX[1]]
+                else
+                    skipping = true
+                end
+            end
+
+            if !skipping
+                # with three nearest points, get the expression of the plane,
+                # then find the z value of f(xq[i], yq[i])
+                D = det(vertices)
+                D = D == 0 ? eps() : D
+                d = 1
+                a = -d/D * det(
+                    hcat([1,1,1], vertices[:, 2:3])
+                )
+                b = -d/D * det(
+                    hcat(vertices[:, 1], [1,1,1], vertices[:, 3])
+                )
+                c = -d/D * det(
+                    hcat(vertices[:, 1:2], [1,1,1])
+                )
+                vq = (a*xq[i] + b*yq[i] + d) / -c
+                append!(res, vq)
+            else
+                append!(res, NaN)
+            end
         else
-            append!(res, NaN)
+                append!(res, NaN)
         end
     end
 
     return res
 
-
 end
 
 """
-        calculate the area of a triangle from its three vertices
-        """
-function areaTriangle(vertices::DataFrame)
-    if size(vertices, 1) != 3
-        error("Triangle should have three vertices")
-    end
-
+    calculate the area of a triangle from its three vertices
+"""
+function areaTriangle(t::GeometricalPredicates.UnOrientedTriangle{Point2D})
     area = abs(1/2 * (
-        vertices.X[1] * (vertices.Y[2] - vertices.Y[3]) +
-        vertices.X[2] * (vertices.Y[3] - vertices.Y[1]) +
-        vertices.X[3] * (vertices.Y[1] - vertices.Y[2])
+        getx(geta(t)) * (gety(getb(t)) - gety(getc(t))) +
+        getx(getb(t)) * (gety(getc(t)) - gety(geta(t))) +
+        getx(getc(t)) * (gety(geta(t)) - gety(getb(t)))
     ))
     return area
-
-
 end
 
 
 """
-        check if a point is within a triangle
-        """
-function isInTriangle(point::DataFrame, vertices::DataFrame)
-    if size(vertices, 1) != 3
-        error("Triangle should have three vertices")
-    end
+    check if a point is within a triangle
+"""
+function isInTriangle(p::Point2D,
+                      t::GeometricalPredicates.UnOrientedTriangle{Point2D})
+    TA = Primitive(p, getb(t), getc(t))
 
-    TA = vcat(vertices[1:2, :], point)
-    TB = vcat(vertices[[1,3], :], point)
-    TC = vcat(vertices[2:3, :], point)
+    TB = Primitive(p, geta(t), getc(t))
+
+    TC = Primitive(p, geta(t), getb(t))
 
     AreaA = areaTriangle(TA)
     AreaB = areaTriangle(TB)
     AreaC = areaTriangle(TC)
 
-    AreaOrigin = areaTriangle(vertices)
+    AreaOrigin = areaTriangle(t)
 
-    return AreaOrigin == AreaA + AreaB + AreaC
+    return AreaOrigin ≈ AreaA + AreaB + AreaC
+end
+
+
+"""
+    Scale and shift an array of data to fit within
+    the interval defined by `min` and `max`
+"""
+function normalizedToInterval(x::Array, min::Real, max::Real)
+    width = max - min
+    dataMax = maximum(x)
+    dataMin = minimum(x)
+    scale  = (dataMax- dataMin) / width
+    res = x ./ scale
+    shift = minimum(res) - min
+    res .-= shift
+    return res, scale, shift
+end
+
+
+"""
+    reverse the array of data according to `scale` and `shift`
+"""
+function reverseNormalizedToInterval(x::Array, scale::Real, shift::Real)
+
+    res = x .+ shift
+    res .*= scale
+
+    return res
+
 end
---
+++ testscipt.jl
@@ -0,0 +1,49 @@
+using LinearAlgebra
+using VoronoiDelaunay
+
+
+x = -3 .+ 6 .* rand(50000);
+y = -3 .+ 6 .* rand(50000);
+v = sin.(x).^4 .* cos.(y);
+xq = 1;yq = 2
+
+max = max_coord
+min = min_coord
+
+newX, xScale, xShift = normalizedToInterval(x, min, max)
+newY, yScale, yShift = normalizedToInterval(y, min, max)
+
+backX = reverseNormalizedToInterval(newX, xScale, xShift)
+backX == x
+
+tess = DelaunayTessellation(50)
+
+points = Point.(x, y)
+
+push!(tess, points)
+
+triangles = []
+for dTriangle in tess
+    push!(triangles, dTriangle)
+end
+
+function normalizedToInterval(x::Array, min::Real, max::Real)
+    width = max - min
+    dataMax = maximum(x)
+    dataMin = minimum(x)
+    scale  = (dataMax- dataMin) / width
+    res = x ./ scale
+    shift = minimum(x) - min
+    res .-= shift
+    return x, scale, shift
+end
+
+
+function reverseNormalizedToInterval(x::Array, scale::Real, shift::Real)
+
+    res = x .+ shift
+    res .*= scale
+
+    return x
+
+end
