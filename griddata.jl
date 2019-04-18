674f7f3edbd361ed5c22b71ccfbe574d37b2ea49
熊天锴 <Xiong@xiongtiankais-MacBook-Pro.local>
2019-04-18 17:30:27 +0800

    implemented

--- griddata.jl
+++ griddata.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
-using VoronoiDelaunay
-using GeometricalPredicates
+using DataFrames
+using Triangle
 
 """
         griddata with linear interpolation which works similar to its matlab counterpart
@@ -9,6 +9,11 @@ using GeometricalPredicates
 """
 function griddata(x::Array, y::Array, v::Array,
                   xq, yq)
+
+    # check at most three points aroud the quest point for triangle
+    k = 4
+
+
     if !(length(x) == length(y) && length(y) == length(v))
         error("x, y and v must be in same dimension.")
     end
@@ -16,27 +21,35 @@ function griddata(x::Array, y::Array, v::Array,
         error("xq and yq must be in same dimension.")
     end
 
-    newX, xScale, xShift = normalizedToInterval(x, VoronoiDelaunay.min_coord,
-                                                VoronoiDelaunay.max_coord)
-    newY, yScale, yShift = normalizedToInterval(y, VoronoiDelaunay.min_coord,
-                                                VoronoiDelaunay.max_coord)
+    points = [x y]
+    points_map = collect(1:length(x))
 
-    tess = DelaunayTessellation(2*length(x))
-    points = Point.(newX, newY)
-    push!(tess, points)
-    dict = Dict(zip(points, collect(1:length(x))))
+    dists = Array{Float64, 1}(undef, length(x))
+    df = DataFrame(idx = points_map, X = x, Y = y, V = v, Dist = dists)
+    triangles = Triangle.basic_triangulation(points, points_map)
     res = Array{Float64, 1}(undef, length(xq))
     for i = 1:length(xq)
-        normalizedX = xq[i] / xScale - xShift
-        normalizedY = yq[i] / yScale - yShift
-        p = Point2D(normalizedX, normalizedY)
-        theTriangle = locate(tess, p)
-        if !isexternal(theTriangle)
+        p = [xq[i], yq[i]]
+        # get the distances
+        for i = 1:length(dists)
+            dists[i] = ((p[1] - points[i, 1])^2 + (p[2] - points[i, 2])^2)^0.5
+        end
+ 
+        df.Dist = dists
+        sort!(df, :Dist)
+        neighbours = df.idx[1:k]
+        candidates = []
+        for i = 1:k
+            candidates = vcat(candidates, filter(x-> neighbours[i] in x, triangles))
+        end
+
+        theTriangle = locate(candidates, p, df)
+        if theTriangle != nothing
             vertices = Array{Float64, 2}(undef, 3,3)
             # index of vertice A in `points`
-            idxA = dict[geta(theTriangle)]
-            idxB = dict[getb(theTriangle)]
-            idxC = dict[getc(theTriangle)]
+            idxA = theTriangle[1]
+            idxB = theTriangle[2]
+            idxC = theTriangle[3]
             vertices[1,:] = [x[idxA], y[idxA], v[idxA]]
             vertices[2,:] = [x[idxB], y[idxB], v[idxB]]
             vertices[3,:] = [x[idxC], y[idxC], v[idxC]]
@@ -66,17 +79,37 @@ function griddata(x::Array, y::Array, v::Array,
 
 end
 
-"""
-        Scale and shift an array of data to fit within
-        the interval defined by `min` and `max`
-"""
-function normalizedToInterval(x::Array, min::Real, max::Real)
-    width = max - min
-    dataMax = maximum(x)
-    dataMin = minimum(x)
-    scale  = (dataMax- dataMin) / width
-    res = x ./ scale
-    shift = minimum(res) - min
-    res .-= shift
-    return res, scale, shift
+
+function locate(candidates::Array{Any, 1}, p::Array, df::DataFrame)
+
+    for t in candidates
+        if inTriangle(p, t, df)
+            return t
+        end
+    end
+
+    return nothing
+end
+
+function inTriangle(p::Array, t::Array{Int64, 1}, df::DataFrame)
+    pA = convert(Matrix, filter(row->row[:idx] == t[1], df)[[:X, :Y]])
+    pB = convert(Matrix, filter(row->row[:idx] == t[2], df)[[:X, :Y]])
+    pC = convert(Matrix, filter(row->row[:idx] == t[3], df)[[:X, :Y]])
+
+    areaA = areaTriangle(p, pB, pC)
+    areaB = areaTriangle(p, pA, pC)
+    areaC = areaTriangle(p, pA, pB)
+    areaOriginal = areaTriangle(pA, pB, pC)
+    return areaOriginal ≈ areaA + areaB + areaC
+end
+
+
+function areaTriangle(p1::Array, p2::Array, p3::Array)
+    area = abs(1/2 * (
+        p1[1] * (p2[2] - p3[2]) +
+        p2[1] * (p3[2] - p1[2]) +
+        p3[1] * (p1[2] - p2[2])
+    ))
+
+    return area
 end
